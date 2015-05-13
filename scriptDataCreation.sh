183cb85580d1a7cb054d6c558662abfa2e313b1f
loicNorgeot <loic.norgeot@gmail.com>
2015-05-13 11:22:55 +0200

    Facilite la création des fichiers de données

--- scriptDataCreation.sh
+++ scriptDataCreation.sh
@@ -5,8 +5,15 @@
 #PBS -N matrix_creation
 #PBS -j oe 
 
+#Vérification de la validité de l'argument
+if (("$nnz"<10000)) || (("$nnz">10000000000))
+then
+    echo "Error: NNZ must be in range 1e4 - 1e10 "
+    exit
+fi
+
 cp $PBS_O_WORKDIR/makeMatrix.py .
 cp $PBS_O_WORKDIR/csr.py .
-python makeMatrix.py > log.txt
+python makeMatrix.py $nnz
 cp *.txt $PBS_O_WORKDIR
 
